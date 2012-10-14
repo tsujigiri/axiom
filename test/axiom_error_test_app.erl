40c65fea5cf8347874a4fdd66dfcc3a686b8ac18
Helge Rausch <helge@rausch.io>
2012-10-14 14:28:39 +0200

    no default http status for Handler:error/1 for now
    may be re-added later

--- test/axiom_error_test_app.erl
+++ test/axiom_error_test_app.erl
@@ -1,6 +1,8 @@
 -module(axiom_error_test_app).
 -export([start/0, handle/3, error/1]).
 
+-include_lib("axiom/include/response.hrl").
+
 start() ->
 	axiom:start(?MODULE).
 
@@ -8,5 +10,5 @@ handle('GET', [<<"fails">>], _Req) ->
 	foo = bar.
 
 error(_Req) ->
-	<<"custom 500 message">>.
+	#response{status = 500, body = <<"custom 500 message">>}.
 
