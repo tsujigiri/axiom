0194db6d59a1ede8f2fcf1776a09557ad2aad2ae
Helge Rausch <helge@rausch.io>
2013-03-05 10:11:19 +0100

    changing all #http_req to cowboy_req:req()

--- src/axiom.erl
+++ src/axiom.erl
@@ -1,16 +1,15 @@
 -module(axiom).
+-behaviour(cowboy_http_handler).
 
 % callbacks
--export([init/3, handle/2, terminate/2]).
+-export([init/3, handle/2, terminate/3]).
 
 % api
 -export([start/1, start/2, stop/0, params/1, param/2, dtl/1, dtl/2, redirect/2,
-	chunk/2, set_header/3]).
+	chunk/2]).
 
 -record(state, {handler}).
 
--include_lib("cowboy/include/http.hrl").
--include_lib("axiom/include/response.hrl").
 -include_lib("kernel/include/file.hrl").
 
 
@@ -42,7 +41,14 @@ start(Handler, Options) ->
 	lists:map(fun({K,V}) -> application:set_env(?MODULE, K, V) end, Options),
 	{ok, Host} = application:get_env(?MODULE, host),
 	{ok, Path} = application:get_env(?MODULE, path),
-	Dispatch = [{Host, static_dispatch() ++ [{Path, ?MODULE, [Handler]}]}],
+	Dispatch = cowboy_router:compile([{'_', [{'_', ?MODULE, [Handler]},
+						{"/[...]", cowboy_static, [
+								{directory, [<<"public">>]},
+								{mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+								]}]}]),
+	%[{Host, static_dispatch() ++ [{'_', ?MODULE, [Handler]}]}]),
+	ok = application:start(crypto),
+	ok = application:start(ranch),
 	ok = application:start(cowboy),
 	ok = case application:get_env(axiom, sessions) of
 		{ok, _} -> application:start(axiom);
@@ -50,9 +56,8 @@ start(Handler, Options) ->
 	end,
 	{ok, NbAcceptors} = application:get_env(axiom, nb_acceptors),
 	{ok, Port} = application:get_env(axiom, port),
-	cowboy:start_listener(axiom_listener, NbAcceptors,
-		cowboy_tcp_transport, [{port, Port}],
-		cowboy_http_protocol, [{dispatch, Dispatch}]
+	cowboy:start_http(axiom_listener, NbAcceptors, [{port, Port}],
+		[{env, [{dispatch, Dispatch}]}]
 	).
 
 
@@ -104,68 +109,75 @@ dtl(Template, Params) when is_list(Template) ->
 	Response.
 
 
-%% @doc Returns a `response' record, redirecting the client to `UrlOrPath'.
--spec redirect(string(), #http_req{}) -> #response{}.
+%% @doc Redirect the client to `UrlOrPath'.
+-spec redirect(string(), cowboy_req:req()) -> cowboy_req:req().
 redirect(UrlOrPath, Req) ->
 	{ok, UrlRegex} = re:compile("^https?://"),
-	Url = case re:run(UrlOrPath, UrlRegex) of
-		{match, _} -> UrlOrPath;
-		nomatch ->
-			[
-				<<"http">>,
-				case Req#http_req.transport of
-					cowboy_ssl_transport -> <<"s">>;
-					_ -> <<>>
-				end,
-				<<"://">>,
-				join(Req#http_req.host, <<".">>),
-				case Req#http_req.port of
-					80 -> <<>>;
-					Port -> [<<":">>, integer_to_list(Port)]
-				end,
-				UrlOrPath
-			]
+	{Url, Req1} = case re:run(UrlOrPath, UrlRegex) of
+		{match, _} -> {UrlOrPath, Req};
+		nomatch -> assemble_url(UrlOrPath, Req)
 	end,
-	Status = case {Req#http_req.version, Req#http_req.method} of
+	{Method, Req2} = cowboy_req:method(Req1),
+	{Version, Req3} = cowboy_req:version(Req2),
+	Status = case {Version, Method} of
 		{{1,1}, 'GET'} -> 302;
 		{{1,1}, _} -> 303;
 		_ -> 302
 	end,
-	#response{status = Status,
-		headers = [{'Location', binary_to_list(iolist_to_binary(Url))}]}.
+	Req4 = cowboy_req:set_meta(response_status, Status, Req3),
+	cowboy_req:set_resp_header(<<"Location">>, Url, Req4).
 
 
-%% @doc Extracts the request params from the `http_req' record.
--spec params(#http_req{}) -> [tuple()].
+%% @doc Assembles a full URL from a path and a `cowboy_req:req()'.
+-spec assemble_url(iolist(), cowboy_req:req()) -> {iolist(), cowboy_req:req()}.
+assemble_url(Path, Req) ->
+	{Host, Req1} = cowboy_req:host(Req),
+	{Port, Req2} = cowboy_req:port(Req1),
+	Url = [
+		<<"http">>,
+		case cowboy_req:get(transport, Req2) of
+			cowboy_ssl_transport -> <<"s">>;
+			_ -> <<>>
+		end,
+		<<"://">>,
+		join(Host, <<".">>),
+		case Port of
+			80 -> <<>>;
+			Port -> [<<":">>, integer_to_list(Port)]
+		end,
+		Path
+	],
+	{Url, Req2}.
+
+
+%% @doc Extracts the request params from a `cowboy_req:req()'.
+-spec params(cowboy_req:req()) -> {[tuple()], cowboy_req:req()}.
 params(Req) ->
-	element(1, cowboy_http_req:body_qs(Req)) ++
-	element(1, cowboy_http_req:qs_vals(Req)).
+	{ok, BodyQS, Req1} = cowboy_req:body_qs(Req),
+	{QSVals, Req2} = cowboy_req:qs_vals(Req1),
+	{QSVals ++ BodyQS, Req2}.
 
 
-%% @doc extracts a specific param from the `http_req' record.
--spec param(binary(), #http_req{}) -> binary().
+%% @doc extracts a specific param from a `cowboy_req:req()'.
+-spec param(binary(), cowboy_req:req()) -> binary().
 param(Param, Req) ->
 	proplists:get_value(Param, params(Req)).
 
 
-%% @doc Initiates a chunked reply and sends chunked data. The returned
-%% `#http_req{}' of the first call, has to be given as an argument in
-%% subsequent calls and returned from `Handler:handle/3'.
-%%
+%% @doc Initiates a chunked reply and sends chunked data.
 %% If you want to set a Content-Type other than `<<"text/html">>', do
-%% so with the second argument. Otherwise use {@link chunk/2}.
--spec chunk(iodata(), #http_req{}, binary()) -> {ok, #http_req{}}.
+%% so with the third argument. Otherwise use {@link chunk/2}.
+-spec chunk(iodata(), cowboy_req:req(), binary()) -> {ok, cowboy_req:req()}.
 chunk(Data, Req, ContentType) when is_binary(Data) ->
-	Req3 = case Req#http_req.resp_state of
+	Req2 = case cowboy_req:get(resp_state, Req) of
 		waiting ->
-			{ok, Req2} = cowboy_http_req:chunked_reply(200,
+			{ok, Req1} = cowboy_req:chunked_reply(200,
 					[{'Content-Type', ContentType}], Req),
-			Pid = spawn(fun() -> stream_loop(Req2) end),
-			Req2#http_req{meta = 
-				lists:keystore(meta, 1, Req2#http_req.meta, {stream_loop_pid, Pid})};
+			Pid = spawn(fun() -> stream_loop(Req1) end),
+			cowboy_req:set_meta(stream_loop_pid, Pid, Req1);
 		chunks -> Req
 	end,
-	Loop = proplists:get_value(stream_loop_pid, Req3#http_req.meta),
+	{Loop, Req3} = cowboy_req:meta(stream_loop_pid, Req2),
 	Loop ! Data,
 	{ok, Req3}.
 
@@ -174,62 +186,51 @@ chunk(Data, Req) ->
 	chunk(Data, Req, <<"text/html">>).
 
 
-%% @doc Adds a response header field to a `response' or `http_req'
-%% record.
--spec set_header(cowboy_http:header(), binary(), #response{}) -> #response{};
-                (cowboy_http:header(), binary(), #http_req{}) -> #http_req{}.
-set_header(Key, Value, Resp = #response{}) ->
-	Headers = lists:keystore(Key, 1, Resp#response.headers, {Key, Value}),
-	Resp#response{headers = Headers};
-
-set_header(Key, Value, Req = #http_req{}) ->
-	{ok, Req2} = cowboy_http_req:set_resp_header(Key, Value, Req),
-	Req2.
-
-
 %% CALLBACKS
 
 %% @private
-%% @doc Called by cowboy with a `http_req' record and the `state' record
+%% @doc Called by cowboy with a `cowboy_req:req()' and the `state' record
 %% we set in {@link init/3}.
--spec handle(#http_req{}, #state{}) -> {ok, #http_req{}, #state{}}.
+-spec handle(cowboy_req:req(), #state{}) -> {ok, cowboy_req:req(), #state{}}.
 handle(Req, State) ->
 	Handler = State#state.handler,
 	Req2 = axiom_session:new(Req),
-	{Resp, Req3} = try
+	Req3 = try
 		call_handler(Handler, Req2)
 	catch Error:Reason ->
 		handle_error(Error, Reason, erlang:get_stacktrace(), Handler, Req)
 	end,
-	{ok, Req4} = case Req3#http_req.resp_state of
+	{ok, Req5} = case cowboy_req:get(resp_state, Req3) of
 		waiting ->
-			cowboy_http_req:reply(Resp#response.status,
-				Resp#response.headers, Resp#response.body, Req3);
+			{Status, Req4} = cowboy_req:meta(response_status, Req3, 200),
+			cowboy_req:reply(Status, Req4);
 		chunks ->
 			% wait for the streaming loop to finish sending everything
-			Loop = proplists:get_value(stream_loop_pid, Req3#http_req.meta),
+			{Loop, Req4} = cowboy_req:meta(stream_loop_pid, Req3),
 			MonRef = monitor(process, Loop),
 			Loop ! terminate,
 			receive {'DOWN', MonRef, process, Loop, _Info} -> ok
 			after 60000 -> ok
 			end,
-			{ok, Req3};
+			{ok, Req4};
 		_ -> {ok, Req3}
 	end,
-	{ok, Req4, State}.
+	{ok, Req5, State}.
 
 
 %% @private
 %% @doc Called by {@link //cowboy} during initialization.
--spec init({tcp, http}, #http_req{}, [module()]) -> {ok, #http_req{}, #state{}}.
+-spec init({tcp, http}, cowboy_req:req(), [module()]) ->
+	{ok, cowboy_req:req(), #state{}}.
 init({tcp, http}, Req, [Handler]) ->
-	{ok, Req, #state{handler = Handler}}.
+	%{ok, Req, #state{handler = Handler}}.
+	{upgrade, protocol, cowboy_rest}.
 
 
 %% @private
 %% @doc Called by {@link //cowboy} during termination.
--spec terminate(#http_req{}, #state{}) -> ok.
-terminate(_Req, _State) ->
+-spec terminate(term(), cowboy_req:req(), #state{}) -> ok.
+terminate(_Reason, _Req, _State) ->
 	ok.
 
 
@@ -238,59 +239,65 @@ terminate(_Req, _State) ->
 %% @private
 %% @doc Calls the request handler's `before_filter/1', `after_filter/2'
 %% and `handle/3' functions.
--spec call_handler(module(), #http_req{}) -> {#response{}, #http_req{}}.
+-spec call_handler(module(), cowboy_req:req()) -> cowboy_req:req().
 call_handler(Handler, Req) ->
-	Method = Req#http_req.method,
-	Path = Req#http_req.path,
-	Req2 = case erlang:function_exported(Handler, before_filter, 1) of
-		true -> Handler:before_filter(Req);
+	{Method, Req1} = cowboy_req:method(Req),
+	{Path, Req2} = cowboy_req:path(Req1),
+	Req3 = case erlang:function_exported(Handler, before_filter, 1) of
+		true -> Handler:before_filter(Req2);
 		false -> Req
 	end,
-	{Resp, Req3} = process_response(Handler:handle(Method, Path, Req2), Req2),
-	{Resp2, Req4} = case erlang:function_exported(Handler, after_filter, 2) of
-		true -> Handler:after_filter(Resp, Req3);
-		false -> {Resp, Req3}
+	SplitPath = case binary:split(Path, <<"/">>, [global, trim]) of
+		[<<>> | Rest] -> Rest;
+		Else -> Else
 	end,
-	{Resp2, Req4}.
+	io:format("~p", [SplitPath]),
+	Req4 = process_response(Handler:handle(Method, SplitPath, Req3), Req3),
+	Req5 = case erlang:function_exported(Handler, after_filter, 2) of
+		true -> Handler:after_filter(Req4);
+		false -> Req4
+	end,
+	Req5.
 
 
 %% @private
 %% @doc Handles errors in the request handler's `before_filter/1',
 %% `after_filter/2' and `handle/3' functions.
--spec handle_error(atom(), any(), [tuple()], module(), #http_req{}) -> {#response{}, #http_req{}}.
+-spec handle_error(atom(), any(), [tuple()], module(), cowboy_req:req()) ->
+	cowboy_req:req().
 handle_error(error, function_clause, [{Handler, handle, _, _}|_], Handler, Req) ->
-	Path = Req#http_req.path,
-	Resp = #response{status = 404,
-		body = dtl(axiom_error_404, [{path, io_lib:format("~p", [Path])}])},
-	{Resp, Req};
+	{Path, Req1} = cowboy_req:path(Req),
+	Req2 = cowboy_req:set_meta(response_status, 404, Req1),
+	Body = dtl(axiom_error_404, [{path, io_lib:format("~p", [Path])}]),
+	cowboy_req:set_resp_body(Body, Req2);
 
 handle_error(Error, Reason, Stacktrace, Handler, Req) ->
-	{Resp, Req2} = case erlang:function_exported(Handler, error, 1) of
+	Req2 = case erlang:function_exported(Handler, error, 1) of
 		true ->
 			process_response(Handler:error(Req), Req);
-		false -> {
-				#response{status = 500, body = dtl(axiom_error_500, [
-					{error, Error},
-					{reason, io_lib:format("~p", [Reason])},
-					{stacktrace, format_stacktrace(Stacktrace)}
-				])},
-				Req
-			}
+		false -> 
+			Req1 = cowboy_req:set_meta(response_status, 500, Req),
+			Body = dtl(axiom_error_500, [
+						{error, Error},
+						{reason, io_lib:format("~p", [Reason])},
+						{stacktrace, format_stacktrace(Stacktrace)}
+						]),
+			cowboy_req:set_resp_body(Body, Req1)
+
 	end,
-	{Resp, Req2}.
+	Req2.
 
 
 %% @private
-%% @doc Whatever Handler:handle/3 returns, make it a tuple looking like
-%% this: `{#response{}, #http_req{}}'
--spec process_response(#response{}, #http_req{}) -> {#response{}, #http_req{}};
-                      (#http_req{}, #http_req{}) -> {#response{}, #http_req{}};
-					  (iolist(), #http_req{}) -> {#response{}, #http_req{}}.
+%% @doc Processes the output of `Handler:handle/3'
+-spec process_response(cowboy_req:req(), cowboy_req:req()) -> cowboy_req:req();
+	                  (iolist(), cowboy_req:req()) -> cowboy:req().
 
-process_response(Resp = #response{}, Req) -> {Resp, Req};
-process_response(Req = #http_req{}, _) -> {#response{}, Req};
 process_response(Resp, Req) when is_binary(Resp); is_list(Resp) ->
-	{#response{body=Resp}, Req}.
+	cowboy_req:set_resp_body(Resp, Req);
+
+process_response(Req, _) ->
+	Req.
 
 
 %% @private
@@ -316,27 +323,15 @@ atomify_keys([Head|Proplist]) ->
 -spec static_dispatch() -> [tuple()].
 static_dispatch() ->
 	{ok, PubDir} = application:get_env(axiom, public),
-	Files = case file:list_dir(PubDir) of
-		{error, enoent} -> [];
-		{ok, F} -> F
-	end,
-	Dirs = [X || X <- Files, is_directory(X, PubDir)],
-	lists:map(fun(Dir) ->
-			{
-				[list_to_binary(Dir), '...'],
-				cowboy_http_static,
-				[
-					{directory, [list_to_binary(PubDir), list_to_binary(Dir)]},
-					{mimetypes, {fun mimetypes:path_to_mimes/2, default}}
-				]
-			}
-		end, Dirs).
+	Files = filelib:fold_files(PubDir, ".*", true,
+			fun(F,Acc) -> [F -- PubDir|Acc] end, []),
+	lists:foldl(fun(File, Acc) ->
+				[{File, cowboy_static, [
+							{directory, <<"public">>},
+							{mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+							]} | Acc]
+		end, [], Files).
 
-%% @private
--spec is_directory(string(), string()) -> true | false.
-is_directory(F, PubDir) ->
-	{ok, FileInfo} = file:read_file_info([PubDir, "/", F]),
-	FileInfo#file_info.type == directory.
 
 
 %% @private
@@ -359,12 +354,12 @@ format_stacktrace([H|Stacktrace]) ->
 
 
 %% @private
--spec stream_loop(#http_req{}) -> ok.
+-spec stream_loop(cowboy_req:req()) -> ok.
 stream_loop(Req) ->
 	receive
 		terminate -> ok;
 		Data ->
-			ok = cowboy_http_req:chunk(Data, Req),
+			ok = cowboy_req:chunk(Data, Req),
 			stream_loop(Req)
 	end.
 
@@ -377,3 +372,4 @@ join([Bin], _Sep) ->
 join([Bin|Bins], Sep) ->
 	Rest = join(Bins, Sep),
 	<<Bin/binary, Sep/binary, Rest/binary>>.
+
