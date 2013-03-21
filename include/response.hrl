bbb63d0d6687a85bec26799b8286adc2e6e9f7b4
Helge Rausch <helge@rausch.io>
2013-03-21 08:00:28 +0100

    removing response.hrl

--- include/response.hrl
+++
@@ -1,6 +0,0 @@
--record(response, {
-		status = 200                                  :: non_neg_integer(),
-		headers = [{'Content-Type', <<"text/html">>}] :: [tuple()],
-		body = <<>>                                   :: iodata(),
-		req                                           :: cowboy_req:req()
-}).
